75fb5db783682bc1ab428dc6d4f83e65d4d157b2
cabbage16 <baejaehyeon0106@gmail.com>
2023-11-07 16:07:27 +0900

    initial commit

---
+++ src/test/java/com/bamdoliro/wind/WindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bamdoliro.wind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
