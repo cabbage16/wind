14551b1b4e262441ae7103c97a5cdc46e26e975f
cabbage16 <baejaehyeon0106@gmail.com>
2023-11-09 19:03:46 +0900

    ADD :: BaseTimeEntity 바람 생성 날짜

--- src/main/java/com/bamdoliro/wind/domain/BaseTimeEntity.java
+++ src/main/java/com/bamdoliro/wind/domain/BaseTimeEntity.java
@@ -1,4 +1,10 @@
 package com.bamdoliro.wind.domain;
 
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
 public class BaseTimeEntity {
+    @CreatedDate
+    private LocalDateTime createdDate;
 }
