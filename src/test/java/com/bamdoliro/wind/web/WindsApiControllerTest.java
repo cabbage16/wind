cb2cfa389e6e897b0a77a2dd7860e87c865b9630
cabbage16 <baejaehyeon0106@gmail.com>
2023-11-13 17:51:59 +0900

    TEST :: 바람 등록 테스트 바람이 정상적으로 등록되는지 테스트

--- src/test/java/com/bamdoliro/wind/web/WindsApiControllerTest.java
+++ src/test/java/com/bamdoliro/wind/web/WindsApiControllerTest.java
@@ -1,15 +1,23 @@
 package com.bamdoliro.wind.web;
 
+import com.bamdoliro.wind.domain.wind.Winds;
 import com.bamdoliro.wind.domain.wind.WindsRepository;
+import com.bamdoliro.wind.web.dto.WindsSaveRequestDto;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -26,5 +34,29 @@ class WindsApiControllerTest {
     @AfterEach
     public void tearDown() {
         windsRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("바람 등록 테스트")
+    public void winds_register() {
+        //given
+        String content = "content";
+        WindsSaveRequestDto requestDto = WindsSaveRequestDto.builder()
+                .content(content)
+                .build();
+        String url = "http://localhost:" + port + "/api/create";
+
+        //when
+        ResponseEntity<Long> responseEntity = restTemplate
+                .postForEntity(url, requestDto, Long.class);
+
+        //then
+        assertThat(responseEntity.getStatusCode())
+                .isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody())
+                .isGreaterThan(0L);
+
+        List<Winds> all = windsRepository.findAll();
+        assertThat(all.get(0).getContent()).isEqualTo(content);
     }
 }
